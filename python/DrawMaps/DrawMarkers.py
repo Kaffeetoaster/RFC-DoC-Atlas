e71f8e09ee0baa6d4bcb297fa5259654b3fbb116
Anton Geerling <anton.geerling@gmail.com>
2026-04-12 16:04:29 +0200

    fixed resource icons, so that less overwriting happens; refactored DrawReligionMaps

---
+++ python/DrawMaps/DrawMarkers.py
@@ -0,0 +1,222 @@
+from python.consts import *
+
+import config
+
+from PIL import Image, ImageDraw
+from pathlib import Path
+import json
+
+
+def add_marker_config_entry(config_dict, coords, text, path_art, category, bSpawn):
+    entry = {
+        "x": coords[0],
+        "y": coords[1],
+        "display_name": text,
+        "source": str(path_art),
+        "category": category,
+        "spawn": bSpawn # important for color
+    }
+    config_dict["spawns_and_despawns"].append(entry)
+
+
+
+def update_resource_despawn():
+    # updates the resource despawn entries, by getting the reosource, that spawned there before or the starting resource.
+    dRemovedResourcesDictExtended = dRemovedResourcesDict
+    for (x,y), event in dRemovedResourcesDictExtended.items():
+        if (x,y) in dResourcesDict and dResourcesDict[(x,y)][0] < event:
+            # maybe a spawned resource will despawn
+            iresource = dResourcesDict[(x,y)][1]
+            dRemovedResourcesDictExtended[(x,y)] = (event, iresource)
+        else:
+            # otherwise its just the starting resource that despawns
+            dRemovedResourcesDictExtended[(x,y)] = (event, dTileMap[(x, iWorldY-1-y)]["bonus"])
+    return dRemovedResourcesDictExtended
+
+def update_feature_despawn():
+    dRemovedFeaturesDictExtended = dRemovedFeaturesDict
+    for (x,y), event in dRemovedFeaturesDictExtended.items():
+        if (x,y) in dFeaturesDict:
+            # maybe a spawned feature will despawn
+            ifeature = dFeaturesDict[(x,y)][1]
+            dRemovedFeaturesDictExtended[(x,y)] = (event, ifeature)
+        else:
+            # otherwise its just the starting feature that despawns
+            dRemovedFeaturesDictExtended[(x,y)] = (event, dTileMap[(x, iWorldY-1-y)]["feature"])
+    return dRemovedFeaturesDictExtended
+
+def update_Spawnresources():
+    dSpawnResourcesDictExtended = dSpawnResourcesDict
+    for (x,y), event in dSpawnResourcesDictExtended.items():
+        iresource = event[1]
+        iCiv = event[0]
+        desc = LCivXML[iCiv]['Description'] if LCivXML[iCiv]['ShortDescription'].startswith("TXT_KEY_CIV_") else LCivXML[iCiv]['ShortDescription']
+        new_event = f"{dBirth[iCiv]} - {desc} spawn" 
+        dSpawnResourcesDictExtended[(x,y)] = (new_event, iresource)
+    return dSpawnResourcesDictExtended
+
+
+
+#### resource spawns ###
+def draw_resource_spawns(json_config):
+    for coords, event in dResourcesDict.items():
+        iresource = event[1]
+        year = str(event[0])
+        resource_info = LBonusXML[iresource]
+        path_art = resource_info["ArtDefineTag"]
+        old_img = Image.open(path_art)
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+        if old_img.size == (64,64):
+            img = old_img.crop((3,3,60,60))
+            img.save(new_path)
+        add_marker_config_entry(json_config, coords, year, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Resource spawns", bSpawn=True)
+        
+### resource despawns ###
+def draw_resource_despawns(json_config):
+    dRemovedResourcesDictExtended = update_resource_despawn()
+    # print(dRemovedResourcesDictExtended)
+    for coords, event in dRemovedResourcesDictExtended.items():
+        year = str(event[0])
+        iresource = event[1]
+        resource_info = LBonusXML[iresource]
+        path_art = resource_info["ArtDefineTag"]
+
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_despawn.png"
+        old_img = Image.open(path_art)
+        deletion_img = Image.open("Assets/Art/Interface/Buttons/Deletion.png")
+
+        if old_img.size == (64,64):
+            old_img = old_img.crop((3,3,60,60))
+        old_img.paste(deletion_img, (0,0), deletion_img)
+        old_img.save(new_path)
+        add_marker_config_entry(json_config, coords, year, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Resource despawns", bSpawn=False)
+
+## civ spawn resources
+def draw_civ_spawn_resources(json_config):
+    dSpawnResourcesDictExtended = update_Spawnresources()
+    for (x,y), event in dSpawnResourcesDictExtended.items():
+        iresource = event[1]
+        text = event[0]
+        resource_info = LBonusXML[iresource]
+        path_art = resource_info["ArtDefineTag"]
+        old_img = Image.open(path_art)
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+        if old_img.size == (64,64):
+            img = old_img.crop((3,3,60,60))
+            img.save(new_path)
+        add_marker_config_entry(json_config, (x,y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Civ spawn Resources", bSpawn=True)
+
+
+
+### Feature spawns and despawns ###
+def draw_feature_spawns(json_config):
+    for (x,y), event in dFeaturesDict.items():
+        ifeature = event[1]
+        year = str(event[0])
+        feature_info = LFeatureXML[ifeature]
+        text = f"{year} - {feature_info['Description']}"
+        path_art = feature_info["ArtDefineTag"]
+        old_img = Image.open(path_art)
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+        if old_img.size == (64,64):
+            img = old_img.crop((3,3,60,60))
+            img.save(new_path)
+        add_marker_config_entry(json_config, (x,y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Feature spawns and despawns", bSpawn=True)
+
+def draw_feature_despawns(json_config):
+    dRemovedFeaturesDictExtended = update_feature_despawn()
+    for (x,y), event in dRemovedFeaturesDictExtended.items():
+        year = str(event[0])
+        ifeature = event[1]
+        feature_info = LFeatureXML[ifeature]
+        path_art = feature_info["ArtDefineTag"]
+        text = f"{year} - {feature_info['Description']}"
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_despawn.png"
+        old_img = Image.open(path_art)
+        deletion_img = Image.open("Assets/Art/Interface/Buttons/Deletion.png")
+
+        if old_img.size == (64,64):
+            old_img = old_img.crop((3,3,60,60))
+        old_img.paste(deletion_img, (0,0), deletion_img)
+        old_img.save(new_path)
+        add_marker_config_entry(json_config, (x,y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Feature spawns and despawns", bSpawn=False)
+
+### Terrain changes ###
+def draw_terrain_changes(json_config):
+    for (x,y), event in dTerrainsDict.items():
+        iterrain = event[1]
+        year = str(event[0])
+        terrain_info = LTerrainXML[iterrain]
+        text = f"{year} - {terrain_info['Description']}"
+        path_art = terrain_info["ArtDefineTag"]
+        old_img = Image.open(path_art)
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+        if old_img.size == (64,64):
+            img = old_img.crop((4,4,59,59))
+            img.save(new_path)
+        add_marker_config_entry(json_config, (x,y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Terrain changes", bSpawn=True)
+
+### Plot changes ###
+def draw_plot_changes(json_config):
+    for (x,y), event in dConquerorPlotTypesDict.items():
+        iCiv = event[0]
+        text = f"Inca conquerer spawn" 
+        ## be careful only hill spawns are supported right now.
+        old_img = load_from_atlas(["Art/Interface/Buttons/BaseTerrain_TerrainFeatures_Atlas.dds",4,1])
+        
+        if old_img.size == (64,64):
+            img = old_img.crop((4,4,59,59))
+            img.save(config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/Hill_spawn.png")
+
+        add_marker_config_entry(json_config, (x,y), text, Path(config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/Hill_spawn.png").relative_to(config.OUTPUT_PATH), category = "Plot changes", bSpawn=True)    
+
+### Region based resource spawns ###
+def draw_region_based_resource_spawns(json_config):
+    for (x,y), event in dCivGroupResourcesDict.items():
+        tCivs = event[0]
+        iResource = event[1]
+        text = f"{event[2]} - {', '.join([LCivXML[iCiv]['ShortDescription'] for iCiv in tCivs])}"
+        resource_info = LBonusXML[iResource]
+        path_art = resource_info["ArtDefineTag"]
+        old_img = Image.open(path_art)
+        new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+        if old_img.size == (64,64):
+            img = old_img.crop((3,3,60,60))
+            img.save(new_path)
+        add_marker_config_entry(json_config, (x,y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "City in same region", bSpawn=True)
+
+
+### start resources ###
+def draw_start_resources(json_config):
+    for (x,y), Tile in dTileMap.items():
+        if "bonus" in Tile:
+            iResource = Tile["bonus"]
+            resource_info = LBonusXML[iResource]
+            path_art = resource_info["ArtDefineTag"]
+            old_img = Image.open(path_art)
+            new_path = config.OUTPUT_PATH / "Assets/Art/Interface/Buttons/" / f"{Path(path_art).stem}_cropped.png"
+            if old_img.size == (64,64):
+                img = old_img.crop((3,3,60,60))
+                img.save(new_path)
+            text = ""
+            add_marker_config_entry(json_config, (x,iWorldY-1-y), text, Path(new_path).relative_to(config.OUTPUT_PATH), category = "Start resources", bSpawn=True)
+
+
+def draw_tile_markers(json_config):
+    draw_resource_spawns(json_config)
+    draw_resource_despawns(json_config)
+    draw_civ_spawn_resources(json_config)
+    draw_feature_spawns(json_config)
+    draw_feature_despawns(json_config)
+    draw_terrain_changes(json_config)
+    draw_plot_changes(json_config)
+    draw_region_based_resource_spawns(json_config)
+    draw_start_resources(json_config)
+
+
+
+
+
+
+
+
---
+++ python/DrawMaps/DrawReligionMap.py
@@ -0,0 +1,46 @@
+from python.helper.helper import *
+from python.consts import *
+import config
+from PIL import Image
+from pathlib import Path
+
+def getSpreadFactor(iReligion, iRegion):
+	if iRegion < 0: 
+		return -1
+	
+	return next((iFactor for iFactor, lRegions in tSpreadFactors[iReligion].items() if iRegion in lRegions), iNone)
+
+
+def iterate_religion_spread_factors(iReligion):
+	for (x,y), Tile in dTileMap.items():
+		iRegion = Tile.get("region", -1)
+		iSpreadFactor = getSpreadFactor(iReligion, iRegion)
+		if iSpreadFactor == iCore:
+			yield (x, y), CORE
+		
+		elif iSpreadFactor == iHistorical:
+			yield (x, y), HISTORICAL
+		
+		elif iSpreadFactor == iPeriphery:
+			yield (x, y), PERIPHERY
+		
+		elif iSpreadFactor == iMinority:
+			yield (x, y), MINORITY
+		
+
+def draw_religion_map(iReligion, json_config):
+	display_name = LReligionXML[iReligion].get("Description")
+	print(display_name)
+	
+	image = Image.new("RGBA", (iWorldX, iWorldY), (0, 0, 0, 0))
+	pixels = image.load()
+	
+	for (x, y), spread_factor_type in iterate_religion_spread_factors(iReligion):
+		pixels[x, y] = plot_colors[spread_factor_type]
+	
+	image = image.resize((iWorldX * TILE_SIZE, iWorldY * TILE_SIZE), resample=Image.Resampling.NEAREST)
+	
+	image_path = config.OUTPUT_PATH / "maps/layers/Religions" / f"{display_name}.png"
+	image.save(image_path)
+	add_layer_config_entry(json_config, display_name, "religion", Path(image_path).relative_to(config.OUTPUT_PATH), image.size)
+	
