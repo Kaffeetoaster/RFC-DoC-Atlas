af7791e50e8ba019e3d23d8bb526b730884f5819
Anton Geerling <anton.geerling@gmail.com>
2026-04-12 16:06:11 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from python.consts import *  # all relevant game data
-from python.DrawMaps.DrawStabAndReligon import *
+
+from python.DrawMaps.DrawReligionMap import *
+from python.DrawMaps.DrawMarkers import *
+from python.DrawMaps.DrawStabilityMaps import *
 from python.DrawMaps.DrawSpawn import *
 import config
 
+import json
 
 input_path = config.INPUT_PATH
 output_path = config.OUTPUT_PATH
@@ -12,45 +16,62 @@ output_path = config.OUTPUT_PATH
 
 
 if __name__ == "__main__":
-    
-### Draw Stab Maps ###
-    for iCiv in dCivNames:
-        draw_stability_map_for_civ(iCiv)
+    layers_config = {
+        "layers": []
+    }
+### generate tooltip infos and json entries for resource spawn ###
+    markers_config ={
+        "spawns_and_despawns": []  
+    }
+# ### Draw Stab Maps ###
+#     for iCiv in dCivNames:
+#         draw_stability_map_for_civ(iCiv)
         
-        for iPeriod in dCivPeriods.get(iCiv, []):
-            if should_draw_for_period(iPeriod):
-                draw_stability_map_for_period(iCiv, iPeriod)
+#         for iPeriod in dCivPeriods.get(iCiv, []):
+#             if should_draw_for_period(iPeriod):
+#                 draw_stability_map_for_period(iCiv, iPeriod)
 ### Draw Religion Maps ###
     for iReligion in range(iNumReligions):
-        draw_religion_map(iReligion)
+        draw_religion_map(iReligion, layers_config)
 
-### Draw Birth Maps, extended Birth and Respawn too ### 
-    for iCiv in dCivNames:
-        if iCiv in dBirthArea:
-            draw_birth_map(iCiv, dBirthArea, dBirthAreaExceptions, "maps/layers/Spawns")
-        else:
-            draw_birth_map(iCiv, dCoreArea, dCoreAreaExceptions, "maps/layers/Spawns")
+# ### Draw Birth Maps, extended Birth and Respawn too ### 
+#     for iCiv in dCivNames:
+#         if iCiv in dBirthArea:
+#             draw_birth_map(iCiv, dBirthArea, dBirthAreaExceptions, "maps/layers/Spawns")
+#         else:
+#             draw_birth_map(iCiv, dCoreArea, dCoreAreaExceptions, "maps/layers/Spawns")
 
-        for iPeriod in dCivPeriods.get(iCiv, []):
-                if should_draw_for_period(iPeriod):
-                    draw_birth_map(iCiv, dPeriodCoreArea, dPeriodCoreAreaExceptions, "maps/layers/Spawns/Periods",iPeriod)
+#         for iPeriod in dCivPeriods.get(iCiv, []):
+#                 if should_draw_for_period(iPeriod):
+#                     draw_birth_map(iCiv, dPeriodCoreArea, dPeriodCoreAreaExceptions, "maps/layers/Spawns/Periods",iPeriod)
 
-        if iCiv in dExtendedBirthArea:
-            draw_birth_map(iCiv, dExtendedBirthArea, dExtendedBirthAreaExceptions, "maps/layers/Spawns/Extended")
-        if iCiv in dRespawnArea:
-            draw_birth_map(iCiv, dRespawnArea, dRespawnAreaExceptions, "maps/layers/Spawns/Respawns")
+#         if iCiv in dExtendedBirthArea:
+#             draw_birth_map(iCiv, dExtendedBirthArea, dExtendedBirthAreaExceptions, "maps/layers/Spawns/Extended")
+#         if iCiv in dRespawnArea:
+#             draw_birth_map(iCiv, dRespawnArea, dRespawnAreaExceptions, "maps/layers/Spawns/Respawns")
 
-            
 
-### Draw Resource Map ###
- 
+
+    ### Draw Resource Maps ###
+    draw_tile_markers(markers_config)
+
+
+
+
+
+
+    with open("json/layers_config.json", "w") as f:           
+        json.dump(layers_config, f, indent = 2)
+        
+    with open("json/tooltips.json", "w") as f:
+        json.dump(markers_config, f, indent=2)
+
+
 ### Draw UHV Maps ###
 
 
 ### Draw Geography ###
 # Regions
-# Peak changes
 
-### Draw general Resource Map ###
 
 
